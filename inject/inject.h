014160575f96e282adbe8622848c1e02fd1b3f6e
darlingscdn <251584008+darlingscdn@users.noreply.github.com>
2026-04-16 00:43:45 -0500

    Update inject.h

--- inject/inject.h
+++ inject/inject.h
@@ -2,7 +2,28 @@
 #include <windows.h>
 #include <cstdint>
 
-DWORD     pid(const char* name);
-uintptr_t base(HANDLE proc, const char* name);
-uintptr_t cave(HANDLE proc, uintptr_t mod, size_t start, size_t end, size_t len);
-DWORD     hijack(HANDLE proc, DWORD tid, uintptr_t dest, LPVOID vstack);
+class RbxChannel {
+public:
+    static constexpr size_t kHijackN = 55; // MATCH THIS
+
+    bool start();
+    void stop();
+    bool print(const char* msg, uint32_t level);
+    bool alive() const;
+
+private:
+    HANDLE    proc_{};
+    DWORD     pid_{};
+    void*     chan_{};   // worker
+    void*     worker_{}; // stub
+    HANDLE    thread_{};
+
+    bool       use_worker_{true};
+    uintptr_t  cave_{};
+    uint8_t    hijack_tpl_[64]{};
+    uint8_t    hijack_orig_[64]{};
+
+    bool prep(uintptr_t game);
+    bool post(const char* msg, uint32_t level);
+    bool trap(const char* msg, uint32_t level);
+};
