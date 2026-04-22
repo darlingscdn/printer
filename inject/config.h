e759f2532abd0da44521e5680e2e4541d1d32bb6
darlingscdn <251584008+darlingscdn@users.noreply.github.com>
2026-04-22 14:40:58 -0500

    version-2e6461290a3541f5

--- inject/config.h
+++ inject/config.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <cstdint>
 
-constexpr uintptr_t kPrint   = 0x1D96FB0; // use update.exe on new roblox update
+constexpr uintptr_t kPrint   = 0x1DEA630; // use update.exe on new roblox update
 constexpr uint32_t  kLvPrint = 0, kLvInfo = 1, kLvWarn = 2, kLvErr = 3;
 constexpr uint32_t  kLevel   = kLvPrint;
 
